a713594ca2b547ca53b95cb86b32f5ef3bbf5f6e
acapla <zxcvbnm5288@gmail.com>
2024-03-17 17:59:11 +0900

    Remove unnecessary files and dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,12 @@ module.exports = {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  rules: { 'react/jsx-filename-extension': [2, { extensions: ['.jsx'] }] },
+  rules: {
+    'react/jsx-filename-extension': [2, { extensions: ['.jsx'] }],
+    'react/react-in-jsx-scope': 'off', // react 17부터 React import 하지 않아도 JSX 사용할 수 있음
+    'react/button-has-type': 'off', // button type 지정 하지 않아도 됨
+    // 'import/no-extraneous-dependencies': ['error', { devDependencies: ['**/*.jsx'] }],
+  },
   overrides: [
     {
       env: {
---
+++ src/components/Button/index.jsx
@@ -0,0 +1,9 @@
+import PropTypes from 'prop-types';
+
+export default function Button({ children }) {
+  return <button>{children}</button>;
+}
+
+Button.propTypes = {
+  children: PropTypes.node.isRequired,
+};
