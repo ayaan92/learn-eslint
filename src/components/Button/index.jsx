7c46f3f1e3cf963a6a470ccc86d6044ceefdd5ee
ayaan <ayaan@t-win.kr>
2024-03-20 13:51:47 +0900

    Update JSDoc configuration and add jaguarjs-jsdoc package

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -3,7 +3,8 @@ import PropTypes from 'prop-types';
 /**
  * A button component
  *
- * @param {React.ReactNode} children The children of the button
+ * @param {object} props The props for the component
+ * @param {React.ReactNode} props.children The children of the button
  * @returns {JSX.Element} The button component
  */
 export default function Button({ children }) {
